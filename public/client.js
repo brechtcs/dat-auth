81014857e1c9e5fab51bae004bb933524cd2bad2
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2018-06-27 19:24:19 +0100

    Generate token on server to prevent easy exploit

--- public/client.js
+++ public/client.js
@@ -1,17 +1,44 @@
 var form = document.querySelector('form')
+var msg = document.querySelector('main')
 
 form.addEventListener('submit', async function (event) {
   event.preventDefault()
 
-  var profileKey = form.elements.profileKey.value
-  var tokenId = Date.now().toString(32)
+  form.style.display = 'none'
+  msg.style.display = 'block'
+
+  var key = form.elements.profileKey.value
+  var profile, token
 
   try {
-    var profile =  await DatArchive.load(profileKey)
-    await profile.writeFile(`/${tokenId}`, '3600')
-    form.elements.tokenId.value = tokenId
-    form.submit()
+
+    var firstStep = await fetch('/auth/dat', {
+      credentials: 'include'
+    })
+    var firstRes = await firstStep.json()
+
+    token = firstRes.token
+    profile =  await DatArchive.load(key)
+    await profile.writeFile(`/${token}`, '')
+
+    var secondStep = await fetch('/auth/dat', {
+      body: JSON.stringify({profileKey: key}),
+      credentials: 'include',
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    var secondRes = await secondStep.json()
+
+    alert(secondRes.auth ? 'great success!' : 'epic fail.')
   } catch (e) {
     alert(e.toString())
+  } finally {
+    form.style.display = 'block'
+    msg.style.display = 'none'
+
+    profile.unlink(`/${token}`)
   }
 })
--- server.js
+++ server.js
@@ -1,48 +1,88 @@
 var Dat = require('dat-node')
 var express = require('express')
 var parser = require('body-parser')
+var session = require('express-session')
 var static = require('serve-static')
+var uuid = require('uuid/v4')
 
-var app = express()
-app.use(parser.urlencoded())
-app.use(datAuth)
-app.use(static('public'))
-app.listen(5000)
-
-function datAuth (req, res, next) {
-  if (req.url === '/auth/dat') {
-    var profileKey = req.body.profileKey
-    var tokenId = req.body.tokenId
-
-    Dat(`/tmp/dat-auth-${profileKey}`, {
-      key: profileKey,
-      sparse: true
-    }, function (err, dat) {
-      if (err) {
-        res.writeHead(403)
-        res.end(err.message)
-        return
-      }
-      dat.joinNetwork()
-      dat.archive.readFile(`/${tokenId}`, function (err, ttl) {
-        if (err) {
-          res.writeHead(403)
-          res.end(err.message)
-          return
-        }
-        var now = Date.now()
-        var expiry = parseInt(tokenId, 32) + parseInt(ttl)
-
-        if (now < expiry) {
-          res.writeHead(200)
-          res.end('hooray!')
-        } else {
-          res.writeHead(403)
-          res.end('nope.')
-        }
-      })
-    })
-  } else {
-    next()
+var sessionOpts = {
+  secret: 'somethingsomethingblablablah',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false
   }
 }
+
+var app = express()
+app.use(session(sessionOpts))
+app.use(parser.json())
+app.use(static('public'))
+
+app.get('/auth/dat', function (req, res) {
+  req.session.token = uuid()
+  req.session.save(function (err) {
+    if (err) {
+      res.status(500)
+      res.json({
+        auth: false,
+        err: err.message
+      })
+      return
+    }
+    res.status(200)
+    res.json({
+      token: req.session.token
+    })
+  })
+})
+
+app.post('/auth/dat', function (req, res) {
+  var key = req.body.profileKey
+  var token = req.session.token
+  console.info('requested authentication:', key)
+
+  Dat(`/tmp/dat-auth-${key}`, {
+    key: key,
+    sparse: true
+  }, function (err, dat) {
+    if (err) {
+      console.error(err)
+      res.status(500)
+      res.json({
+        auth: false,
+        err: 'Server error'
+      })
+      return
+    }
+    dat.joinNetwork(function (err) {
+      if (err || !dat.network.connected || !dat.network.connecting) {
+        console.warn('failed connection:', key)
+        res.status(401)
+        res.json({
+          auth: false,
+          err: 'Failed to connect to profile'
+        })
+        return
+      }
+      dat.archive.readFile(token, function (err) {
+        if (err) {
+          console.warn('failed authentication:', key)
+          res.status(401)
+          res.json({
+            auth: false,
+            err: 'No valid token found in profile'
+          })
+          return
+        }
+        console.info('successful authentication:', key)
+        res.status(200)
+        res.json({
+          auth: true
+        })
+      })
+    })
+  })
+})
+
+app.listen(5000)
